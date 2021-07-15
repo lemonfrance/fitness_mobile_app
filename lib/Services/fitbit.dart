5e603fd0057445dc6230a34098936029a03280f0
Jaimee <jmcl168@aucklanduni.ac.nz>
2021-07-15 14:53:43 +1200

    no message

--- lib/Services/fitbit.dart
+++ lib/Services/fitbit.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'dart:html';
 import 'package:http/http.dart' as http;
 import 'package:flutter_web_auth/flutter_web_auth.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 
 import 'database.dart';
@@ -14,22 +16,25 @@ class FitBitService {
 
   Future getCode() async {
     const url = 'https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=23B82K&redirect_uri=http%3A%2F%2Flocalhost&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight';
+    String current_url = window.location.href;
 
     final result = await FlutterWebAuth.authenticate(
         url: url,
-        callbackUrlScheme: 'http://localhost');
+        callbackUrlScheme: current_url);
 
     //get auth code
-    final code = Uri.parse(result).queryParameters['code'];
-    getAuthToken(code);
+    return Uri.parse(result).queryParameters['code'];
+    //getAuthToken(code);
   }
 
-  Future<http.Response> getAuthToken(String code) {
-    return http.post(Uri.parse('https://api.fitbit.com/oauth2/token?code=$code&grant_type=authorization_code&redirect_uri=http://localhost'),
+  Future getAuthToken(String code) async{
+    http.Response response = await http.post(Uri.parse('https://api.fitbit.com/oauth2/token?code=$code&grant_type=authorization_code&redirect_uri=http://localhost'),
       headers: {
         'Authorization': 'Basic MjNCODJLOjQ1MTA4ZTY1MDA0MzE2MmIzYThkODdjODNhY2JlOTdj',
         'Content-Type' : 'application/x-www-form-urlencoded',
       });
+    return (jsonDecode(response.body)["access_token"]);
+
   }
 
   Future<http.Response> getRefreshToken() {
